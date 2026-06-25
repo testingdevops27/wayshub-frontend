0bdb211d74eabb45b70c06d14ce12cc9bf3800cf
testingdevops27 <testingdevops27@gmail.com>
2026-06-25 05:49:25 +0000

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "http://103.193.178.218:5000/api/v1"
 });
 
 const setAuthToken = (token) => {
